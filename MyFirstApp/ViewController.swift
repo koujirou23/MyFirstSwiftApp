0d13b19a98f7011f20e994e5e072d8a477e5668c
Kojiro23 <Kojiro23>
2023-04-09 21:32:00 +0900

    簡単なアプリを作成

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var textField1: UITextField!
+    @IBOutlet var label1: UILabel!
+    @IBOutlet weak var botton1: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        label1.text = ""
     }
 
-
+    @IBAction func buttonTapped(_ sender: Any) {
+        let s = textField1.text ?? ""
+        label1.text = "Hello, \(s)!"
+    }
+    
 }
 
